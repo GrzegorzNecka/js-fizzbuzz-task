ec5e15ef36e6cbb42feec480e45f493308d31cc5
GrzegorzNecka <strzypior@gmail.com>
2021-07-01 08:24:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fizzbuzz-task
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fizzbuzz-task)
---
+++ index.js
@@ -0,0 +1,8 @@
+import fizzBuzz from './fizzBuzz';
+
+const fizzBuzzExecute = fizzBuzz([[3, 'fizz'], [5, 'buzz']]);
+
+
+for (let i = 0 ; i>10; i++) {
+  
+}
