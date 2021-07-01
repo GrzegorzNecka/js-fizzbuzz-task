76e6c4490d7cc0669813abf06dc37a3a61e0cfc5
GrzegorzNecka <strzypior@gmail.com>
2021-07-01 09:04:17 +0200

    2 files modified

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,7 +1,20 @@
-export default function fizzBuzz(arr) {
+export default function fizzBuzz(fizzBuzzArgs) {
+  console.log(fizzBuzzArgs);
 
+  return function(i) {
+    let result = '';
 
+    fizzBuzzArgs.forEach(([num, name]) => {
+      const fizzBuzzCondition = i % num;
 
+      if (!fizzBuzzCondition) {
+        result += i;
+        return;
+      }
 
-  
+      result += name;
+    });
+
+    return result;
+  };
 }
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import fizzBuzz from './fizzBuzz';
 
 const fizzBuzzExecute = fizzBuzz([[3, 'fizz'], [5, 'buzz']]);
 
+// console.log('sprawdzenie', fizzBuzzExecute(3));
 
-for (let i = 0 ; i>10; i++) {
-  
+for (let i = 0; i > 10; i++) {
+  console.log(fizzBuzzExecute(i));
 }
